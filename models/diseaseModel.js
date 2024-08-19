fe150c31b5b3ac250b107e2e076459884b01b251
eranbabayov <eranbabayoveran@gmail.com>
2024-08-19 21:38:18 +0300

    save
    save

--- models/diseaseModel.js
+++ models/diseaseModel.js
@@ -5,7 +5,9 @@ async function getAllDiseases() {
     try {
         console.log("Try to get diseases from db")
         const connection = await db.get_connection(); // Using the connect function from data_base.js
-        const result = await connection.request().query('SELECT DISTINCT ChronicCondition FROM Patients');
+        const result = await connection.request().query(`SELECT ChronicCondition FROM Patients
+                                                        GROUP BY ChronicCondition HAVING CHARINDEX(',', ChronicCondition) = 0 
+                                                        AND ChronicCondition IS NOT NULL;`);
         return result.recordset;
     } catch (err) {
         console.error('Error querying the database:', err);
